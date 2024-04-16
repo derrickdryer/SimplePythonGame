aa1528c7b372495ff434fe4b855c46e1f1868ba1
Derrick Dryer <derrickdryer@proton.me>
2024-04-15 21:59:12 -0400

    A lot of reworking from the original dev branch. Created various functions to enable csv imports along with the graphics for them. Reanimated player sprite with a much simpiler method by splitting the player sprite into various states. Added functions for future use such as attacking and magic.

--- level.py
+++ level.py
@@ -3,6 +3,8 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
+from random import choice
 
 class Level:
     def __init__(self):
@@ -14,18 +16,43 @@ class Level:
         self.create_map()
     
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y), [self.visible_sprites, self.obstacles_sprites])
-                if col == 'p':
-                    self.player = Player((x,y), [self.visible_sprites], self.obstacles_sprites)
+        layout = {
+            'boundary' : import_csv_layout('./assets/map/map_Boundary.csv')
+        }
+        graphics = {
+            'temp' : import_folder('./assets/graphics/Boundary')
+        }
+        print(graphics)
+        for style,layout in layout.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y), [self.visible_sprites, self.obstacles_sprites], 'boundary', graphics['temp'][0])
+                        if style == 'grass':
+                            #random_gradd_image = choice(graphics['grass'])
+                            #Tile((x,y), [self.visible_sprites], 'grass', random_grass_image)
+                            pass
+                        if style == 'object':
+                            #surf = graphics['object'][int(col)]
+                            #Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
+                            pass
+        #for row_index, row in enumerate(WORLD_MAP):
+        #    for col_index, col in enumerate(row):
+        #        x = col_index * TILESIZE
+        #        y = row_index * TILESIZE
+        #        if col == 'x':
+        #            Tile((x,y), [self.visible_sprites, self.obstacles_sprites])
+        #        if col == 'p':
+        #            self.player = Player((x,y), [self.visible_sprites], self.obstacles_sprites)
+        self.player = Player((100,100), [self.visible_sprites], self.obstacles_sprites)
     
     def run(self):
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        debug(self.player.status)
 
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
@@ -34,10 +61,17 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_width = self.display_surface.get_size()[0] / 2
         self.half_height = self.display_surface.get_size()[1] / 2
         self.offset = pygame.math.Vector2()
+
+        self.floor_surf = pygame.image.load('./assets/tilemap/floor.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
     
     def custom_draw(self, player):
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
+
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from support import import_folder
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacles_sprites):
@@ -8,28 +9,92 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         self.hitbox = self.rect.inflate(0, -10)
         
+        self.import_player_assets()
+        self.status = 'down'
+        self.frame_index = 0
+        self.animation_speed = 0.09
+
+        # Movement
         self.direction = pygame.math.Vector2(0, 0)
-        self.speed = 5;
+        self.speed = 5
+        self.attacking = False
+        self.attacking_cooldown = 500
+        self.attack_time = None
         
         self.obstacles_sprites = obstacles_sprites
     
     def input(self):
-        keys = pygame.key.get_pressed()
-        
-        if keys[pygame.K_UP]:
-            self.direction.y = -1
-        elif keys[pygame.K_DOWN]:
-            self.direction.y = 1
-        else:
-            self.direction.y = 0
-        
-        if keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-        elif keys[pygame.K_LEFT]:
-            self.direction.x = -1
-        else:
-            self.direction.x = 0
+        if not self.attacking:
+            keys = pygame.key.get_pressed()
+            
+            # Movement
+            if keys[pygame.K_UP] or keys[pygame.K_w]:
+                self.direction.y = -1
+                self.status = 'up'
+            elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                self.direction.y = 1
+                self.status = 'down'
+            else:
+                self.direction.y = 0
+            
+            if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                self.direction.x = 1
+                self.status = 'right'
+            elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
+                self.direction.x = -1
+                self.status = 'left'
+            else:
+                self.direction.x = 0
+            
+            # Attack
+            if keys[pygame.K_SPACE]:
+                self.attacking = True
+                self.attack_time = pygame.time.get_ticks()
+                print('Attack!')
+
+            # Magic
+            if keys[pygame.K_LSHIFT]:
+                self.attacking = True
+                self.attack_time = pygame.time.get_ticks()
+                print('Magic!')
     
+    def get_status(self):
+        if self.direction.x == 0 and self.direction.y == 0:
+            if not 'idle' in self.status and not 'attack' in self.status:
+                self.status = 'idle_' + self.status
+        
+        if self.attacking:
+            self.direction.x = 0
+            self.direction.y = 0
+            if not 'attack' in self.status:
+                if 'idle' in self.status:
+                    self.status = self.status.replace('idle_', 'attack_')
+                else:
+                    self.status = 'attack_' + self.status
+        else:
+            if 'attack' in self.status:
+                self.status = self.status.replace('attack_', '')
+
+    def animate(self):
+        animation = self.animations[self.status]
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+        
+        self.image = animation[int(self.frame_index)]
+        self.rect = self.image.get_rect(center = self.hitbox.center)
+
+    def import_player_assets(self):
+        character_path = './assets/sprites/player'
+        self.animations = {'up': [], 'down': [], 'left': [], 'right': [],
+            'idle_up': [], 'idle_down': [], 'idle_left': [], 'idle_right': [],
+            'attack_up': [], 'attack_down': [], 'attack_left': [], 'attack_right': []}
+        
+        for animation in self.animations.keys():
+            full_path = character_path + '/' + animation
+            self.animations[animation] = import_folder(full_path)
+            #print(self.animations)
+
     def move(self, speed):
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
@@ -59,4 +124,13 @@ class Player(pygame.sprite.Sprite):
     
     def update(self):
         self.input()
+        self.cooldowns()
+        self.get_status()
+        self.animate()
         self.move(self.speed)
+
+    def cooldowns(self):
+        current_time = pygame.time.get_ticks()
+        if self.attacking and self.attack_time is not None:  # Check if self.attack_time is not None
+            if current_time - self.attack_time >= self.attacking_cooldown:
+                self.attacking = False
